f86b8cdec53fcb4877482e0b68f15abb696fc57b
ArisandiYudiarta <arisandiyudiarta21@gmail.com>
2021-12-09 21:33:04 +0800

    nambahkan beberapa file

---
+++ events.js
@@ -0,0 +1,10 @@
+const p3 = document.querySelector('p.p3');
+
+// function ubahWarna(){
+//     p3.style.backgroundColor = 'cyan';
+// }
+
+p3.onclick = function ubahWarna(){
+    p3.style.backgroundColor = 'cyan';
+}
+
