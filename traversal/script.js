931240e889b29b02f590f94d033c5ef9d46a7b79
ArisandiYudiarta <arisandiyudiarta21@gmail.com>
2021-12-16 13:25:40 +0800

    prevent default n event bubbling

--- traversal/script.js
+++ traversal/script.js
@@ -1,27 +1,51 @@
-//DOM Manipulation
+// //DOM Manipulation
 
-// const card = document.querySelector('div.card');
-// const del = document.querySelector('div.card .close');
+// // const card = document.querySelector('div.card');
+// // const del = document.querySelector('div.card .close');
 
-// del.addEventListener('click', function(){
-//     card.style.display = 'none';
-// })
+// // del.addEventListener('click', function(){
+// //     card.style.display = 'none';
+// // })
 
-//DOM Traversal
+// //DOM Traversal
 
-const x = document.querySelectorAll('.close');
+// const x = document.querySelectorAll('.close');
 
-// for(let i = 0; i < x.length; i++){
-//     x[i].addEventListener('click', function(e){
-//         // e.parentElement.style.display='none';
-//         e.parentElement.target.style.display='none';
+// // for(let i = 0; i < x.length; i++){
+// //     x[i].addEventListener('click', function(e){
+// //         // e.parentElement.style.display='none';
+// //         e.parentElement.target.style.display='none';
+// //     });
+// // }
+
+// //Better solution
+
+// x.forEach(function(o){
+//     o.addEventListener('click', function(e){
+//         e.target.parentElement.style.display='none';
+//         // METHOD PREVENT DEFAULT
+//         e.preventDefault();
+//         //STOP EVENT BUBBLING
+//         e.stopPropagation();
 //     });
-// }
+// });
 
-//Better solution
+// const cards = document.querySelectorAll('.card');
+// cards.forEach(function(card){
+//     card.addEventListener('click', function(e){
+//         alert('ok');
+//     });
+// });
 
-x.forEach(function(o){
-    o.addEventListener('click', function(e){
-        e.target.parentElement.style.display='none';
-    });
+
+// EVENT BUBBLING PRACTICE
+
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function(e){
+    // console.log(e.target);
+    if( e.target.className == 'close' ){
+        e.target.parentElement.style.display = 'none';
+        e.preventDefault();
+    }  
 });
